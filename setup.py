356c5b2d21891270c20dd0e3602c9eee6388aeec
Yeongil Choe <yichoe@yujinrobot.com>
2017-08-04 13:25:33 +0900

    update

--- setup.py
+++ setup.py
@@ -223,8 +223,8 @@ setuptools.setup(name='pyros_interfaces_ros',
     install_requires=[
         'tblib',  # this might not always install six (latest version does not)
         'six',
-        'pyzmq',
-        'pyzmp>=0.0.14',  # lets match the requirement in package.xml (greater than)
+        #'pyzmq',
+        #'pyzmp>=0.0.14',  # lets match the requirement in package.xml (greater than)
         'pyros_setup>=0.1.5',  # Careful : pyros-setup < 0.0.8 might already be installed as a deb in /opt/ros/indigo/lib/python2.7/dist-packages/
         'pyros_config>=0.1.4',
         'pyros-common>=0.4.2',
